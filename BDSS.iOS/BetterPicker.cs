e44de94b4666c9885df476ee4187b96dd57b7f9a
BlueStuLondon <78082139+BlueStuLondon@users.noreply.github.com>
2021-03-05 12:32:48 +0000

    blah

---
+++ BDSS.iOS/BetterPicker.cs
@@ -0,0 +1,22 @@
+﻿
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(PickerRenderer))]
+
+namespace BDSS.iOS
+{
+    public class Picker : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TextAlignment = UITextAlignment.Center;
+            }
+        }
+    }
+}
