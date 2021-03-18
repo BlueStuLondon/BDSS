c8a59d8e9d1b9cceea16176e57c31c3894f515c8
BlueStuLondon <78082139+BlueStuLondon@users.noreply.github.com>
2021-03-18 16:26:58 +0000

    * .DS_Store: * MainActivity.cs: * BDSS.Android.csproj: * Resource.designer.cs:
    * BDSS.sln: message

--- BDSS.Android/MainActivity.cs
+++ BDSS.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Octane.Xamarin.Forms.VideoPlayer.Android;
 
 namespace BDSS.Droid
 {
@@ -21,6 +22,7 @@ namespace BDSS.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FormsVideoPlayer.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
